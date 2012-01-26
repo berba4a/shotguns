8f51eb0e8aa361d50b87c18c7c97e963d9a960fa
berba4a <berba4a@abv.bg>
2012-01-26 15:10:33 +0200

    edit functions

--- controller/templates.controller.php
+++ controller/templates.controller.php
@@ -15,27 +15,27 @@
 			parent::display('html/templates/home_pistols.tpl');
 		}
 		function home_optics() {
-			parent::display('html/templates/home_optics.tpl');
+			parent::display('html/templates/optics/home_optics.tpl');
 		}
 		
 		function results_optics() {
-			parent::display('html/templates/results_optics.tpl');
+			parent::display('html/templates/optics/results_optics.tpl');
 		}
 		
 		function add_optics() {
-			parent::display('html/templates/add_optics.tpl');
+			parent::display('html/templates/optics/add_optics.tpl');
 		}
 		
 		function search_optics() {
-			parent::display('html/templates/search_optics.tpl');
+			parent::display('html/templates/optics/search_optics.tpl');
 		}
 		
 		function preview_optics() {
-			parent::display('html/templates/preview_optics.tpl');
+			parent::display('html/templates/optics/preview_optics.tpl');
 		}
 		
 		function add_bullets() {
-			parent::display('html/templates/add_bullets.tpl');
+			parent::display('html/templates/bullets/add_bullets.tpl');
 		}
 		function search_select_category() {
 			parent::display('html/templates/search_select_category.tpl');
